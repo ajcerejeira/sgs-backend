878404492620c54a73c3e6b37d9fb9c85d0770cf
Afonso Silva <ajcerejeira@gmail.com>
2019-01-26 17:16:43 +0000

    Fixed bug on geocoder

--- src/accidents/geocoder.service.ts
+++ src/accidents/geocoder.service.ts
@@ -10,8 +10,8 @@ export class GeocoderService {
 
   constructor(private readonly httpService: HttpService) {}
 
-  async getAddress(lat: number, lng: number): Promise<string> {
+  async getAddress(lat: number, lng: number): Promise<string | null> {
     const url = `${this.url}/${this.format}?latlng=${lat},${lng}&key=${this.key}`;
-    return this.httpService.get(url).toPromise().then(res => res.data.results[0].formatted_address);
+    return this.httpService.get(url).toPromise().then(res => res.data.results[0].formatted_address || null);
   }
 }
