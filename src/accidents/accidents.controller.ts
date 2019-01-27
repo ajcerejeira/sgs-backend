82a9585cfd150fc29d4584471e7f9a6f2ae62352
Afonso Silva <ajcerejeira@gmail.com>
2019-01-27 12:36:02 +0000

    Updated report generation

--- src/accidents/accidents.controller.ts
+++ src/accidents/accidents.controller.ts
@@ -89,8 +89,8 @@ export class AccidentsController {
   @ApiNotFoundResponse({ description: 'Accident not found' })
   async accidentReport(@Param('id') id: number, @Res() res: Response) {
     const accident = await this.accidentsService.detail(id);
-    res.render('report.hbs', { accident });
-    /*res.render('report.hbs', { accident }, (err, html) =>
+    // res.render('report.hbs', { accident });
+    res.render('report.hbs', { accident }, (err, html) =>
       pdf
         .create(html, {
           format: 'A4',
@@ -105,6 +105,6 @@ export class AccidentsController {
           res.contentType('application/pdf');
           res.end(buffer);
         }),
-    );*/
+    );
   }
 }
